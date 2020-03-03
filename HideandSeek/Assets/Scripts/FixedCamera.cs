5a898de4998d8922bd982f330290c8a598dd3756
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-03-02 22:04:53 -0500

    made the camera move with the player and the player move

---
+++ HideandSeek/Assets/Scripts/FixedCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(-9,2,0);
+    }
+}
---
+++ HideandSeek/Assets/Scripts/PLayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerControl : MonoBehaviour
+{
+    private int walkingSpeed= 9;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        { 
+            transform.Translate(Vector3.right * Time.deltaTime * walkingSpeed); 
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * walkingSpeed);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * walkingSpeed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * walkingSpeed);
+        }
+    }
+}
